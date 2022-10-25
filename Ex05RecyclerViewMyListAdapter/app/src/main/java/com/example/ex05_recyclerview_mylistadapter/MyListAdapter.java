b073c2bf1a51b0b80c3263e7187ea41f2d678866
kingdog2 <91170825+kingdog2@users.noreply.github.com>
2022-10-25 23:42:51 +0800

    Add files via upload

---
+++ Ex05RecyclerViewMyListAdapter/app/src/main/java/com/example/ex05_recyclerview_mylistadapter/MyListAdapter.java
@@ -0,0 +1,64 @@
+package com.example.ex05_recyclerview_mylistadapter;
+
+import android.graphics.text.TextRunShaper;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder> {
+    MyListData[] listdata;
+    public MyListAdapter(MyListData[] listdata){
+        this.listdata = listdata;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater =LayoutInflater.from(parent.getContext());
+        View listItem = layoutInflater.inflate(R.layout.list_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final MyListData myListData = listdata[position];
+        holder.textView.setText(listdata[position].getDescription());
+        holder.imageView.setImageResource(listdata[position].getImageId());
+
+        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(view.getContext(), "click on item"+myListData.getDescription())
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listdata.length;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView imageView;
+        public TextView textView;
+        public RelativeLayout relativeLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            this.textView = (TextView) itemView.findViewById(R.id.textView);
+            relativeLayout = (RelativeLayout) itemView.findViewById(R.id.relativeLayout);
+        }
+    }
+}
---
+++ Ex05RecyclerViewMyListAdapter/app/src/main/java/com/example/ex05_recyclerview_mylistadapter/MyListData.java
@@ -0,0 +1,27 @@
+package com.example.ex05_recyclerview_mylistadapter;
+
+public class MyListData {
+    private String description;
+    private int imageId;
+
+    public MyListData(String description, int imageId){
+        this.description = description;
+        this.imageId = imageId;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+}
