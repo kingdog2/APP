90a15c845221a1f248ee866bcc3169400ea86e60
kingdog2 <91170825+kingdog2@users.noreply.github.com>
2023-11-08 10:58:56 +0800

    Add files via upload

---
+++ time/app/src/main/java/com/example/time/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.time;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePicker();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+    }
+
+    @Override
+    public void onTimeSet(android.widget.TimePicker view, int hourOfDay, int minute) {
+        textView.setText(String.format("%02d", hourOfDay) + ":" + String.format("%02d", minute));
+    }
+}
